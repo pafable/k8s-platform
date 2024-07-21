0bbedd186e70ed5e526d535e808a7906d1214402
pafable <philafable@gmail.com>
2024-07-21 18:05:27 -0400

    added jenkins seed

--- terraform/modules/jenkins/jcasc.tf
+++ terraform/modules/jenkins/jcasc.tf
@@ -49,11 +49,8 @@ locals {
         ]
 
         security = {
-          scriptApproval = {
-            approvedScriptHashes = [
-              "SHA512:b2ce6c70defc8b0c35d412b5ec9c09fabb66443a61cbed9606f62f42d0e5fb601ab7f3ddeb3b5aee006f952dd7e7795b76dd22d1182656dc49a5b8e1e7a00e84",
-              "SHA512:6c985b8810ea05eca5881306b0a87cadbef3030e480000f36fb0ade96b8a4e8bf746acfb28d41fdf1f5a4f06a00dd9cbbd8307687b8f2e274977f06b31524290"
-            ]
+          globalJobDslSecurityConfiguration = {
+            useScriptSecurity = false
           }
         }
 
