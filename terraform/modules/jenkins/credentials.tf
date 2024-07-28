3bc602bc1424647cd09b53ec10593db97b4da360
pafable <philafable@gmail.com>
2024-07-28 12:04:36 -0400

    fix creds

---
+++ terraform/modules/jenkins/credentials.tf
@@ -0,0 +1,26 @@
+locals {
+  domain_credentials = [
+    {
+      credentials = [
+        {
+          usernamePassword = {
+            description = "docker hub credentials"
+            id          = "docker-hub-creds"
+            password    = sensitive(var.docker_hub_password)
+            scope       = "GLOBAL"
+            username    = var.docker_hub_username
+          }
+        },
+        {
+          aws = {
+            accessKey   = sensitive(var.aws_dev_deployer_access_key)
+            description = "AWS credentials for dev account"
+            id          = "aws-dev-deployer-creds"
+            scope       = "GLOBAL"
+            secretKey   = sensitive(var.aws_dev_deployer_secret_key)
+          }
+        }
+      ]
+    }
+  ]
+}
