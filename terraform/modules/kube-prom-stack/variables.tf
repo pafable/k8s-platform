73e8eb9aaaa88095e748dbe279b91cf188855704
pafable <philafable@gmail.com>
2024-08-13 08:00:00 -0400

    updated kube prom stack

--- terraform/modules/kube-prom-stack/variables.tf
+++ terraform/modules/kube-prom-stack/variables.tf
@@ -6,7 +6,7 @@ variable "app_name" {
 variable "chart_version" {
   description = "The version of the Helm chart to install"
   type        = string
-  default     = "61.3.1"
+  default     = "61.8.0"
 }
 
 variable "grafana_image_repo" {
@@ -18,7 +18,7 @@ variable "grafana_image_repo" {
 variable "grafana_image_tag" {
   description = "Grafana image tag"
   type        = string
-  default     = "11.1.0"
+  default     = "11.1.3"
 }
 
 variable "is_cloud" {
