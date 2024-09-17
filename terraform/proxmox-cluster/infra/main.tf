61273fd48956d463ccd3516349002bb7778f1752
pafable <philafable@gmail.com>
2024-09-17 14:41:44 -0400

    update proxmox 3

--- terraform/proxmox-cluster/infra/main.tf
+++ terraform/proxmox-cluster/infra/main.tf
@@ -2,6 +2,7 @@ module "k3s_master" {
   source  = "../../modules/proxmox_vm"
   clone   = "orc-template"
   iso     = "OracleLinux-R9-U4-x86_64-boot.iso"
+  memory  = 4096
   name    = "uruk-hai-01"
   os_type = "cloud-init"
   tags    = "phil,test"
