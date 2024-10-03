c3e9323856173419bb21b29d8aecd7664f16338d
pafable <philafable@gmail.com>
2024-10-03 18:25:18 -0400

    added checker

---
+++ checkers.py
@@ -0,0 +1,14 @@
+from proxmoxer import ProxmoxAPI
+
+
+class ProxSession():
+    def __init__(self):
+        pass
+
+
+class Mock(): pass
+
+
+if __name__ == '__main__':
+    p = ProxSession()
+    print(p.)
