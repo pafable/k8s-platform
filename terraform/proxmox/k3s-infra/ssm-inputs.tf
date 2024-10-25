f001252a66dcf4a0735810cdacfaeb97683ced39
pafable <philafable@gmail.com>
2024-10-25 07:00:35 -0400

    consolidated images

--- terraform/proxmox/k3s-infra/ssm-inputs.tf
+++ terraform/proxmox/k3s-infra/ssm-inputs.tf
@@ -2,3 +2,8 @@ data "aws_ssm_parameter" "k3s_join_token" {
   provider = aws.parameters
   name     = "/proxmox/join/token"
 }
+
+data "aws_ssm_parameter" "main_proxmox_node" {
+  provider = aws.parameters
+  name     = "/proxmox/node/behemoth/ipv4"
+}
