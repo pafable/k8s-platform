dedcb105a46986c78f6211d26849f06f7f3904de
pafable <39135798+pafable@users.noreply.github.com>
2024-12-14 21:34:07 -0500

    added discord bot 3 (#51)

---
+++ terraform/apps/ssm-inputs.tf
@@ -0,0 +1,19 @@
+data "aws_ssm_parameter" "aws_dev_access_key_id" {
+  provider = aws.parameters
+  name     = "/account/dev/aws/deployer/access/key/id"
+}
+
+data "aws_ssm_parameter" "aws_dev_secret_key" {
+  provider = aws.parameters
+  name     = "/account/dev/aws/deployer/secret/access/key"
+}
+
+data "aws_ssm_parameter" "discord_id" {
+  provider = aws.parameters
+  name     = "/discord/server/id"
+}
+
+data "aws_ssm_parameter" "discord_token" {
+  provider = aws.parameters
+  name     = "/discord/server/token"
+}
