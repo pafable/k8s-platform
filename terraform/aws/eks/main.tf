931f893fc027f3a43e4d7a8dc89a83f123f0d342
pafable <philafable@gmail.com>
2025-01-16 19:23:35 -0500

    reenabled public access

--- terraform/aws/eks/main.tf
+++ terraform/aws/eks/main.tf
@@ -16,12 +16,13 @@ module "k8s_eks" {
   source = "../../modules/eks"
   ## uncomment client_vpn_security_group_id to enable client vpn access
   #   client_vpn_security_group_id = data.aws_ssm_parameter.client_vpn_security_group_id.value
-  cluster_name       = local.cluster_name
-  intra_subnet_ids   = jsondecode(data.aws_ssm_parameter.intra_subnet_ids.value)
-  node_desired_size  = 1
-  private_subnet_ids = jsondecode(data.aws_ssm_parameter.private_subnet_ids.value)
-  sso_role_arn       = data.aws_ssm_parameter.aws_sso_role_arn.value
-  vpc_id             = data.aws_ssm_parameter.vpc_id.value
+  cluster_name         = local.cluster_name
+  enable_public_access = true
+  intra_subnet_ids     = jsondecode(data.aws_ssm_parameter.intra_subnet_ids.value)
+  node_desired_size    = 1
+  private_subnet_ids   = jsondecode(data.aws_ssm_parameter.private_subnet_ids.value)
+  sso_role_arn         = data.aws_ssm_parameter.aws_sso_role_arn.value
+  vpc_id               = data.aws_ssm_parameter.vpc_id.value
 
   ## sets EKS worker node instance types
   #   instance_types = toset(local.worker_node_instance_types)
