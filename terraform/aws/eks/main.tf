d668d1084c403354758b424eda1e9fd244e6f77c
pafable <philafable@gmail.com>
2025-01-17 20:25:10 -0500

    updated to 3

--- terraform/aws/eks/main.tf
+++ terraform/aws/eks/main.tf
@@ -17,9 +17,9 @@ module "k8s_eks" {
   ## uncomment client_vpn_security_group_id to enable client vpn access
   #   client_vpn_security_group_id = data.aws_ssm_parameter.client_vpn_security_group_id.value
   cluster_name         = local.cluster_name
-  enable_public_access = false
+  enable_public_access = true
   intra_subnet_ids     = jsondecode(data.aws_ssm_parameter.intra_subnet_ids.value)
-  node_desired_size    = 1
+  node_desired_size    = 3
   private_subnet_ids   = jsondecode(data.aws_ssm_parameter.private_subnet_ids.value)
   sso_role_arn         = data.aws_ssm_parameter.aws_sso_role_arn.value
   vpc_id               = data.aws_ssm_parameter.vpc_id.value
