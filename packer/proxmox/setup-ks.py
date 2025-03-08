f2990ed39dbfb382199eceeb223b337e92461317
pafable <philafable@gmail.com>
2025-03-08 08:43:34 -0500

    created move script

---
+++ packer/proxmox/setup-ks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import shutil
+
+KS_FILE = "ks-srv.service"
+DEST = "/usr/lib/systemd/system/"
+
+
+def copy_srv(src: str, dst: str) -> None:
+    print(shutil.copy2(KS_FILE, DEST))
+
+
+def main():
+    copy_srv(KS_FILE, DEST)
+
+
+if __name__ == "__main__":
+    main()
