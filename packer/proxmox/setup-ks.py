244f3acdf3d46da23a49abb8ee3338ddfb330a6e
pafable <philafable@gmail.com>
2025-03-08 08:58:53 -0500

    created move script

--- packer/proxmox/setup-ks.py
+++ packer/proxmox/setup-ks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import shutil
+import subprocess
+
 
 KS_FILE = "ks-srv.service"
 DEST = "/usr/lib/systemd/system/"
@@ -8,6 +10,10 @@ DEST = "/usr/lib/systemd/system/"
 
 def copy_srv(src: str, dst: str) -> None:
     print(f"copied {KS_FILE} to {shutil.copy2(KS_FILE, DEST)}")
+
+
+def reload_daemon() -> None:
+    print(subprocess.run(["systemctl", "daemon-reload"]))
 
 
 def main():
