8b80e835757ba9a997906494a5e794a3bee4ed9c
pafable <philafable@gmail.com>
2025-03-18 18:56:27 -0400

    updated k3s

--- terraform/modules/jenkins/jenkins.tf
+++ terraform/modules/jenkins/jenkins.tf
@@ -253,7 +253,7 @@ resource "kubernetes_persistent_volume_v1" "jenkins_pv" {
 
     persistent_volume_source {
       nfs {
-        path   = "/volume2/fs"
+        path   = "/volume2/fs/jenkins"
         server = var.nfs_ipv4
       }
     }
