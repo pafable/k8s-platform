f060752c77aa78ea82c8aab1edbcb3b61623a80c
pafable <philafable@gmail.com>
2025-03-23 15:57:35 -0400

    updated dns

--- packer/proxmox/packer-scripts/00-customization.sh
+++ packer/proxmox/packer-scripts/00-customization.sh
@@ -17,6 +17,7 @@ cloud-init clean
 # move repo and customization files to correct dirs
 find /tmp -name '*.repo' -exec mv -fv '{}' /etc/yum.repos.d/ \;
 find /tmp -name 'colorls.sh' -exec mv -fv '{}' /etc/profile.d/ \;
+find /tmp -name 'jail.local' -exec cp -f '{}' /etc/fail2ban/jail.local \;
 find /tmp -name 'vimrc' -exec cp -f '{}' /root/.vimrc \;
 find /tmp -name 'vimrc' -exec mv -fv '{}' /home/${MY_USER}/.vimrc \;
 
@@ -33,3 +34,6 @@ chown -R root: /etc/yum.repos.d
 chown -R root: /etc/profile.d
 chown -R root: /root
 chown -R ${MY_USER}: /home/${MY_USER}
+
+systemctl start fail2ban
+systemctl enable fail2ban
