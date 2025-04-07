5abb40a9b151284addc97be810a0a743e39702ce
pafable <philafable@gmail.com>
2025-04-06 23:03:18 -0400

    updated env

--- terraform/modules/vault/volumes.tf
+++ terraform/modules/vault/volumes.tf
@@ -1,29 +1,29 @@
-locals {
-  access_mode = "ReadWriteOnce"
-}
-
-resource "kubernetes_persistent_volume_v1" "vault_pv" {
-  metadata {
-    name   = "${local.app_name}-pv"
-    labels = local.tf_labels
-  }
-
-  spec {
-    access_modes                     = [local.access_mode]
-    persistent_volume_reclaim_policy = "Retain"
-    storage_class_name               = var.storage_class_name
-    capacity = {
-      storage = "50Gi"
-    }
-
-    persistent_volume_source {
-      nfs {
-        path   = "/volume2/fs/vault"
-        server = "kraken.fleet.pafable.com"
-      }
-    }
-  }
-}
+# locals {
+#   access_mode = "ReadWriteOnce"
+# }
+#
+# resource "kubernetes_persistent_volume_v1" "vault_pv" {
+#   metadata {
+#     name   = "${local.app_name}-pv"
+#     labels = local.tf_labels
+#   }
+#
+#   spec {
+#     access_modes                     = [local.access_mode]
+#     persistent_volume_reclaim_policy = "Retain"
+#     storage_class_name               = var.storage_class_name
+#     capacity = {
+#       storage = "50Gi"
+#     }
+#
+#     persistent_volume_source {
+#       nfs {
+#         path   = "/volume2/fs/vault"
+#         server = "kraken.fleet.pafable.com"
+#       }
+#     }
+#   }
+# }
 
 # resource "kubernetes_persistent_volume_claim_v1" "jenkins_pvc" {
 #   metadata {
