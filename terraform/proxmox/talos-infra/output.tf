25bcfada1e5b6f42b00a997f04e02073af0ced75
pafable <philafable@gmail.com>
2025-12-21 06:05:16 -0500

    update

--- terraform/proxmox/talos-infra/output.tf
+++ terraform/proxmox/talos-infra/output.tf
@@ -2,9 +2,9 @@ output "controller_01_ip" {
   value = module.talos_controller_1.ipv4_address
 }
 
-# output "controller_02_ip" {
-#   value = module.talos_controller_2.ipv4_address
-# }
+output "controller_02_ip" {
+  value = module.talos_controller_2.ipv4_address
+}
 
 output "worker_01_ip" {
   value = module.talos_worker_1.ipv4_address
