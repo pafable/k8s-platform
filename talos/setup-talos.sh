3660779e62b7d0c4f8ae93741cd0011e8b3f3dfc
pafable <philafable@gmail.com>
2025-12-21 06:56:19 -0500

    update

--- talos/setup-talos.sh
+++ talos/setup-talos.sh
@@ -127,6 +127,7 @@ kubectl get all \
 
 echo "Waiting 60 sec for nodes to reboot and kubernetes resources to be ready"
 sleep 60
+
 # get nodes
 kubectl get nodes \
   --kubeconfig="${CONFIG_DIR}"/kubeconfig
