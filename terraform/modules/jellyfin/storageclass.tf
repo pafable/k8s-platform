d3cfef847a9d34298a1213181330ebb473f5ff8c
pafable <philafable@gmail.com>
2026-01-31 08:55:45 -0500

    updated worker 1 to use 8gb of mem

---
+++ terraform/modules/jellyfin/storageclass.tf
@@ -0,0 +1,13 @@
+resource "kubernetes_storage_class_v1" "kraken_nfs_sc" {
+  metadata {
+    name = "kraken-movies-shows"
+  }
+
+  reclaim_policy      = "Retain"
+  storage_provisioner = "nfs.csi.k8s.io"
+
+  parameters = {
+    server = "kraken.fleet.pafable.com"
+    share  = "/volume1/movies-shows"
+  }
+}
