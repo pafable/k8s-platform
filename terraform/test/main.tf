b537fde7d93fcf65e3f0a7399e247bc56c40e728
pafable <philafable@gmail.com>
2026-02-08 11:21:04 -0500

    fixed test

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -10,10 +10,6 @@ variable "config_context" {
 
 module "jellyfin" {
   source = "../modules/jellyfin"
-  controller_ips = [
-    nonsensitive(data.aws_ssm_parameter.talos_controller1_ipv4.insecure_value),
-    nonsensitive(data.aws_ssm_parameter.talos_controller2_ipv4.insecure_value)
-  ]
   nfs_ipv4 = nonsensitive(data.aws_ssm_parameter.nfs_server_ip.insecure_value)
   domain   = "jellyfin-test.home.pafable.com"
 }
